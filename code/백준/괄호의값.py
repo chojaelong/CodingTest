a8eba44f3e39060e94610f8d81add58f2c30e97f
chojaelong <chojaelong@gmail.com>
2022-06-23 15:26:44 +0900

    [2022.06.23][JaeHoCho] 문제 업로드

--- code/백준/괄호의값.py
+++ code/백준/괄호의값.py
@@ -1,9 +1,39 @@
-array = list(input())
+bracket = list(input())
 
 stack = []
+answer = 0
+tmp = 1
 
-for value in array:
-    if value == '(':
-        stack.append(2)
-    elif value == ')':
+for i in range(len(bracket)):
+
+    if bracket[i] == "(":
+        stack.append(bracket[i])
+        tmp *= 2
+
+    elif bracket[i] == "[":
+        stack.append(bracket[i])
+        tmp *= 3
+
+    elif bracket[i] == ")":
+        if not stack or stack[-1] == "[":
+            answer = 0
+            break
+        if bracket[i-1] == "(":
+            answer += tmp
         stack.pop()
+        tmp //= 2
+
+    else:
+        if not stack or stack[-1] == "(":
+            answer = 0
+            break
+        if bracket[i-1] == "[":
+            answer += tmp
+
+        stack.pop()
+        tmp //= 3
+
+if stack:
+    print(0)
+else:
+    print(answer)
