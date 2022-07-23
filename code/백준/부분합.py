b4210f9ddabe04446ae7d77617761ec7f4921dbc
JaeHo.Cho <chojaelong@gmail.com>
2022-07-23 20:28:08 +0900

    [2022-07-23][JaeHo.Cho] 코드 업로드

---
+++ code/백준/부분합.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1806
+
+n, s = map(int, input().split())
+array = list(map(int, input().split()))
+
+answer = 1000001
+for i in range(n - 1):
+    for j in range(i+1, n):
+        if sum(array[i:j+1]) == s:
+            answer = min(len(array[i:j]), answer)
+
+print(answer)
