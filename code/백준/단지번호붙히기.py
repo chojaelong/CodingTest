bc86e13e773128df36461fbb36741ac877b353d7
JaeHo.Cho <chojaelong@gmail.com>
2022-08-03 00:16:42 +0900

    [2022-08-03][JaeHo.Cho] 코드 업로드

---
+++ code/백준/단지번호붙히기.py
@@ -0,0 +1,41 @@
+# https://www.acmicpc.net/problem/2667
+from collections import deque
+
+def bfs(i, j):
+    global n
+    # 동 서 남 북
+    move = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+    array[i][j] = 0
+    queue = deque([(i, j)])
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + move[i][0]
+            ny = y + move[i][1]
+
+            if nx >= n or nx < 0 or ny >= n or ny < 0:
+                continue
+
+            if array[nx][ny] == 1:
+                queue.append((nx, ny))
+                array[nx][ny] = 0
+                count += 1
+    
+    return count
+        
+
+n = int(input())
+array = [list(map(int, input())) for _ in range(n)]
+
+answer = []
+for x in range(n):
+    for y in range(n):
+        if array[x][y] == 1:
+            value = bfs(x, y)
+            answer.append(value)
+
+answer.sort()
+for i in answer:
+    print(i)
