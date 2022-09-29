f105b569243e05a5e52ab34a79f3e6f1a368cbe2
JaeHo.Cho <chojaelong@gmail.com>
2022-09-29 10:37:43 +0900

    [2022-09-29][JaeHo.Cho] 코드 업로드

---
+++ code/프로그래머스/배열자르기.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/87390#
+
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right + 1):
+        quotient = i // n # 1
+        remainder = i % n # 4
+        
+        if quotient + 1 >= remainder:
+            answer.append(quotient + 1)
+        else:
+            answer.append(remainder)
+    
+    return answer
+            
+solution(4, 7, 14)
