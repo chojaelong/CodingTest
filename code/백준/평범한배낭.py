706d541b3efa4c5f04980fe7e6a84930705917ff
JaeHo.Cho <chojaelong@gmail.com>
2022-11-05 22:47:28 +0900

    [2022-11-05][JaeHo.Cho] 냅섹, 누적합 공부

--- code/백준/평범한배낭.py
+++ code/백준/평범한배낭.py
@@ -1,3 +1,4 @@
+'''
 import sys
 read = sys.stdin.readline
 
@@ -9,3 +10,23 @@ for _ in range(N):
     for j in range(K, w-1, -1):
         cache[j] = max(cache[j], cache[j-w] + v)
 print(cache[-1])
+'''
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+array = [[0, 0]]
+for _ in range(n):
+    array.append(list(map(int, input().split())))
+dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    weight = array[i][0]
+    value = array[i][1]
+    for j in range(1, k + 1):
+        if j < weight:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+
+print(dp[n][k])
