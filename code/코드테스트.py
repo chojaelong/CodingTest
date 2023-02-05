27d18ea41cb9ad820b467c8c2ba9072f0a85f7b6
JaeHo.Cho <chojaelong@gmail.com>
2023-02-05 14:31:31 +0900

    [2023-02-05][JaeHo.Cho] 코드트리 문제 디버깅 테스트

--- code/코드테스트.py
+++ code/코드테스트.py
@@ -1,4 +1,24 @@
-from collections import deque
-from dateutil.relativedelta import relativedelta
+n = int(input())
+array = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
 
-print(ord('a'))
+def back_tracking(x, y, value):
+    global answer
+    # 위, 오른쪽
+    dx = [-1, 0]
+    dy = [0, 1]
+
+    # 종료 조건
+    if x == 0 and y == n - 1:
+        answer = max(answer, value)
+        return
+
+    for i in range(2):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < n:
+            back_tracking(nx, ny, value + array[nx][ny])
+
+back_tracking(n - 1, 0, array[n - 1][0])
+print(answer)
